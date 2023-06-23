1e3272071a74635f1403b7e12c73f918d51a72cf
Sumayya Mariyam <sumayyamariyam2005@gmail.com>
2023-06-23 10:19:45 -0400

    Initial Commit

---
+++ ProjectOne/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  InteractiveUIs
+//
+//  Created by Scholar on 6/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var name = ""
+    @State private var textTitle = "I am Sumayya. Your fellow Kode With Klossy Scholar! This is my first app! I am really excited!!"
+        
+    var body: some View {
+        ZStack {
+            Color(.purple)
+        
+        VStack {
+            Text("\(textTitle)")
+                .font(.title)
+            
+            Image("bb")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode:.fit)
+                .padding()
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            
+            Spacer()
+            
+            Button("Click to know more about me") {
+                textTitle = "I love cats. They are the best. I also love to garden with my mom when she is free. My favorite subjects incluse Mathematics and Astronomy. Calculus is the best (when I understand it ofcourse!)"
+                
+                Image("bb")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode:.fit)
+                    .padding()
+                    .imageScale(.large)
+                
+            }
+            .padding()
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.pink)
+            
+            
+            
+        } // closes the VStack
+    }
+    } //closes the body
+} //closes the struct ContentView
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    } //closes the static var previews
+} //closes the struct ContentView_Previews
+
+
+
---
+++ ProjectOne/ProjectOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOneApp.swift
+//  ProjectOne
+//
+//  Created by Scholar on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
